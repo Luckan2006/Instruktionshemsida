3afe5fb2d367b6bf35c2bf85dff00abcfff55aa9
Luckan2006 <77390072+Luckan2006@users.noreply.github.com>
2025-05-17 20:55:45 +0200

    Update and rename Python_Elevator.html to EndProject.php

---
+++ EndProject.php
@@ -0,0 +1,65 @@
+<?php
+session_start();
+if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
+    // Spara var användaren ville ta sig innan man loggar in
+    $_SESSION['redirect_after_login'] = $_SERVER['REQUEST_URI'];
+    header("Location: login.html");
+    exit;
+}
+?>
+<!DOCTYPE html> 
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Övning</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<header>Python</header>
+    <div class="navbar">
+        <button><a href="index.php">Startsida</a></button>
+        <button><a href="Introduction.php">Introduktion</a></button>
+        <button><a href="Print.php">Print</a></button>
+        <button><a href="Class.php">Klasser</a></button>
+        <button><a href="Functions.php">Funktioner</a></button>
+        <button><a href="If-statement.php">If-statser</a></button>
+        <button><a href="EndProject.php">Python hiss övning</a></button>
+    </div>
+    <div class="container">
+        <h1>Detta är slutuppgiften</h1>
+        <p>Rekommenderas att ha gått igenom genomgångarna innan man gör uppgiften.
+        </p>
+        <h2>Instruktioner</h2>
+        <p>En hiss ska programmeras. Den ska ha 10 våningar och göras med hjälp av det du lärt dig från genomgångarna.
+            Hissen ska ränka de våningar som den paserar och den ska skriva ut när man anlänt till våningen man valde. 
+            Ett tilläggspaket för Python som kan vara användbart i denna uppgift är <b>time</b>.
+            För att installera paketet gör som på bilderna nedan. I terminalen och i dokumentet där hissen ska programeras i.
+        </p>
+        <img src="/python_guide/python_images/image28.png" alt="Bild på kod">
+        <p>Skriv in detta i terminalen/konsolen och vänta på att paketet ska installeras.
+        </p>
+        <img src="/python_guide/python_images/image29.png" alt="Bild på kod">
+        <p>Skriv in detta i dokumentet som du ska programmera hissen i längst upp. 
+            Använd kod raden <b>time.Sleep(2)</b> i din kod där du anser att det är lämpligt för att exempelvis tiden för varje våning tar lite tid. 
+            <b>2</b> är 2 sekunder och kan bytas mot den tid man anser är lämplig. 
+            När Du känner dig färdig eller tycker att uppgiften inte går att lösa kan du kolla på koden nedan och analysera hur den kan se ut. 
+        </p>
+        <div class="image-wrapped">
+            <img src="/python_guide/python_images/image30.png" alt="Bild på kod">
+            <div class="overlay">Klicka för att visa</div>
+        </div>
+        <p>Om du har gjort på något annat sätt men att det funkar liknande kan du skicka koden med hjälp av kontakta oss på startsidan så kan det läggas upp här.</p>
+    </div>
+<script src="footer.js" defer></script>
+<script>
+        document.addEventListener("DOMContentLoaded", function () {
+        document.querySelectorAll('.image-wrapped').forEach(function(e) {
+            e.addEventListener('click', function() {
+                e.classList.add('revealed');
+            });
+        });
+    });
+</script>
+</body>
+</html>
